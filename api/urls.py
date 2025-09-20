0110b870144f67f2d77e53367bad9e9285e292b1
Eshadul <md.esadulhaq01@gmail.com>
2025-09-20 09:42:35 +0600

    Implemented Class Based View, all Methods

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('students/',views.students),
     path('students/<int:pk>/',views.studentsDetailsView),
+
+    path('employees/',views.Employees.as_view()),
+    path('employees/<int:pk>/',views.EmployeesDetailsView.as_view()),
 ]
---
+++ employees/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from api.serializers import EmployeeSerializer
+# Create your views here.
+
